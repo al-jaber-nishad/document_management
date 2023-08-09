bac76521ee18bc53e50ac886aa7bece66570a395
al-jaber-nishad <mailnishad02@gmail.com>
2023-08-10 03:29:42 +0600

    Remove unused files that were planned to be

--- authentication/views/permission_views.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ObjectDoesNotExist
-
-from rest_framework import serializers, status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-
-
